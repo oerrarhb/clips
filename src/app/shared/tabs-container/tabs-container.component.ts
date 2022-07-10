86624b6c0a8844c45713c142c1601e5dbce63fa7
Othmane ERRARHBI <othmane.errarhbi@deveryware.com>
2022-07-10 09:21:03 +0200

    refactor modal to tabs

---
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -0,0 +1,24 @@
+import {
+  Component,
+  OnInit,
+  QueryList,
+  AfterContentInit,
+  ContentChildren,
+} from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
+
+@Component({
+  selector: 'app-tabs-container',
+  templateUrl: './tabs-container.component.html',
+  styleUrls: ['./tabs-container.component.css'],
+})
+export class TabsContainerComponent implements AfterContentInit {
+  @ContentChildren(TabComponent)
+  tabs: QueryList<TabComponent> = new QueryList();
+
+  constructor() {}
+
+  ngAfterContentInit(): void {
+    console.log(this.tabs);
+  }
+}
